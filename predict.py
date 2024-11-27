2a7a7239b9184d73d3ae6b420b1400a0ac6f8f80
Olaoluwa Tunmise <oluwatunmise1olaoluwa@gmail.com>
2024-11-27 01:18:07 +0100

    trader_classifier

--- predict.py
+++ predict.py
@@ -1,4 +1,5 @@
 import pickle
+from sklearn.preprocessing import StandardScaler
 
 model_file = 'logistic_regression_model.bin'
 
@@ -8,10 +9,35 @@ with open(model_file, 'rb') as f_in:
 
 print("Model loaded successfully!")
 
-# Verify by predicting on the test set
-y_pred_loaded = model.predict(X_test)
+trader = { 
+    'user_address': '0x09dc02dfb7de2b150fe9a2d2ab92cf5767d423f9', 
+    'base_cumulative_return': -0.952871878, 
+    'portfolio_return': 19.799527694, 
+    'daily_sharpe_ratio': 1.300632733, 
+    'number_of_trades': 36.0, 
+    'unique_tokens_traded': 6.0, 
+    'trader_class_numeric': 1
+    }
 
-# Compare predictions from the original and loaded models
-assert (y_pred == y_pred_loaded).all(), "Mismatch between original and loaded model predictions!"
+# Convert trader dictionary to a 2D array with the same feature order as during training
+features = ['base_cumulative_return', 'portfolio_return', 
+            'daily_sharpe_ratio', 'number_of_trades', 'unique_tokens_traded']
 
-print("Loaded model predictions match the original!")
+
+
+# Create a list of values in the correct order
+trader_values = [[trader[feature] for feature in features]]
+print(trader_values)
+
+#Normalize the Features
+scaler = StandardScaler()
+X = scaler.fit_transform(trader_values)
+
+# Apply scaling
+trader_scaled = scaler.transform(X)
+
+# Predict the probabilities for each class
+prediction_prob = model.predict_proba(trader_scaled)
+
+print('input', trader)
+print(f"Prediction Probabilities: {prediction_prob}")
